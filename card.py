abac6e2d35a1124c7989a63aa00f0a78ae9b49e1
Nick Ellinas <ngreek54@gmail.com>
2018-06-05 20:30:32 +0300

    Cards in cardholders interchangeable

--- card.py
+++ card.py
@@ -2,12 +2,16 @@ import time
 import pygame
 from pygame.locals import *
 
-from card_utils import deactivate
+from card_utils import redraw
+
+HOLDER1 = -3
+HOLDER2 = -2
 
 
 class Card(object):
-    def __init__(self, emotion, picture, cardback, position, cardsize, gamerunner):
-        self.emotion = emotion
+    def __init__(self, number, picture, cardback, position, cardsize, gamerunner, cardholder=False,
+                 color=(200, 200, 200)):
+        self.number = number
         self.picture = picture
         self.cardback = cardback
         self.position = position
@@ -15,46 +19,94 @@ class Card(object):
         self.gamerunner = gamerunner
         self.chosen = False
         self.can_open = True
+        self.cardholder = cardholder
+        self.color = color
+
+        self.img = pygame.image.load(self.picture)
+        self.img = pygame.transform.scale(self.img, (self.cardsize[0], self.cardsize[1]))
+        self.cardholder_positions = ((0, 0), (0, 0))
+        self.in_cardholder = 0
+
+        self.current_position = self.position
 
     def draw(self, screen):
-        if self.chosen:
-            return
-        img = pygame.image.load(self.cardback)
-        img = pygame.transform.scale(img, (self.cardsize[0], self.cardsize[1]))
-        screen.blit(img, self.position)
-        pygame.display.flip()
-        print(self.position)
-        self.rect = Rect(self.position[0], self.position[1], self.cardsize[0], self.cardsize[1])
-        self.screen = screen
+        if self.cardholder:
+            pygame.draw.rect(screen, self.color, Rect([self.position[0], self.position[1]], self.cardsize), 3)
+            pygame.display.flip()
+            self.rect = Rect(self.current_position[0], self.current_position[1], self.cardsize[0], self.cardsize[1])
+            self.screen = screen
+        else:
+            screen.blit(self.img, self.current_position)
+            pygame.display.flip()
+            # print(self.position)
+            self.rect = Rect(self.current_position[0], self.current_position[1], self.cardsize[0], self.cardsize[1])
+            self.screen = screen
 
-    def load_emotion_image(self):
-        img = pygame.image.load(self.picture)
-        self.screen.blit(img, self.position)
-        pygame.display.flip()
+    def chose_number(self, i):
+        self.current_position = self.cardholder_positions[i]
+        self.in_cardholder = i
+
+    def reset_number(self):
+        self.current_position = self.position
+        i = self.in_cardholder
+        self.in_cardholder = 0
+        return i
 
     def clear(self):
         img = pygame.image.load('emotion_recognition_game_data/black.jpg')
         self.screen.blit(img, self.position)
         pygame.display.flip()
 
-    def process_event(self, event, cards, screen):
+    def process_event(self, event, cards, screen, game_dict):
         global previous_time
         mouse_pos = pygame.mouse.get_pos()
         inside = self.rect.collidepoint(mouse_pos)
         if inside:
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and self.chosen == False and self.can_open == True:
-                self.chosen = True
+                cards[-1].clear()
+                # WHEN THE USER SELECTS A CARD FROM THE TOP
                 previous_time = time.time()
-                self.load_emotion_image()
-                self.gamerunner.send_event('athena.games.emorec.card_opened', 'emorec_game', self.emotion)
-                # time.sleep(5)
-                deactivate(cards, screen)
-
-
-class CardHolder(object):
-    def __init__(self, position, cardsize):
-        self.position = position
-        self.cardsize = cardsize
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, [200, 200, 200], Rect([0, 0], self.cardsize), 3)
+                if game_dict['cardholders_full'] == 0:
+                    self.chosen = True
+                    self.chose_number(0)
+                    cards[HOLDER1].chosen = True  # Card in first cardholder
+                    game_dict['cardholders_full'] += 1
+                    game_dict['current_sum'] += self.number
+                    # self.gamerunner.send_event('athena.games.emorec.card_opened', 'emorec_game', self.number)
+                elif game_dict['cardholders_full'] == 1 and cards[HOLDER1].chosen:
+                    self.chosen = True
+                    self.chose_number(1)
+                    cards[HOLDER2].chosen = True  # Card in second cardholder
+                    game_dict['cardholders_full'] += 1
+                    game_dict['current_sum'] += self.number
+                elif game_dict['cardholders_full'] == 1 and cards[HOLDER2].chosen:
+                    self.chosen = True
+                    self.chose_number(0)
+                    cards[HOLDER1].chosen = True  # Card in first cardholder
+                    game_dict['cardholders_full'] += 1
+                    game_dict['current_sum'] += self.number
+                else:
+                    print('Cardholders full!')
+                if game_dict['cardholders_full'] == 2:  # Calculate sum and evaluate
+                    if game_dict['current_sum'] == 4:
+                        cards[-1].color = (0, 200, 0)
+                        print('Correct!')
+                    else:
+                        cards[-1].color = (200, 0, 0)
+                        print('Wrong!')
+                redraw(cards, screen)
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1 and self.chosen == True and self.can_open == True:
+                previous_time = time.time()
+                if game_dict['cardholders_full'] > 0:
+                    self.chosen = False
+                    i = self.reset_number()
+                    if i == 0:
+                        cards[HOLDER1].chosen = False  # Card removed from first cardholder
+                    else:
+                        cards[HOLDER2].chosen = False  # Card removed from second cardholder
+                    game_dict['cardholders_full'] -= 1
+                    game_dict['current_sum'] -= self.number
+                    cards[-1].color = (0, 0, 0)
+                    redraw(cards, screen)
+                else:
+                    print('All cardholders empty!')
--- sums_game.py
+++ sums_game.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 import os
 import time
 
-from card import Card, CardHolder
+from card import Card
 from card_utils import activate, redraw
 
 STOP_EMOREC = USEREVENT + 2
@@ -27,48 +27,114 @@ def start_sums_game(screen, gamerunner):
     w, h = pygame.display.get_surface().get_size()
     print(w, h)
     # Card width
-    cw = int(0.14 * w)
+    cw = int(0.15 * w)
     # Card height
-    ch = int(0.2 * h)
+    ch = int(0.3 * h)
     # Space between cards
-    s = int((0.8 * w - 5 * cw) / 6)
+    s = int((0.9 * w - 5 * cw) / 6)
     cards = []
 
-    card_posx = int(0.1 * w + s)  # + cw / 2)
-    card_posy = 0.2 * h
-    card = Card("happiness", "sums_game_data/zero.gif", cardback,
+    card_posx = int(0.05 * w + s)  # + cw / 2)
+    card_posy = 0.1 * h
+    card = Card(0, "sums_game_data/zero.gif", cardback,
                 (card_posx, card_posy), (cw, ch), gamerunner)
     card.draw(screen)
     cards.append(card)
 
     card_posx += cw + s
-    card = Card("fear", "sums_game_data/one.gif", cardback,
+    card = Card(1, "sums_game_data/one.gif", cardback,
                 (card_posx, card_posy), (cw, ch), gamerunner)
     card.draw(screen)
     cards.append(card)
 
     card_posx += cw + s
-    card = Card("disgust", "sums_game_data/two.gif", cardback,
+    card = Card(2, "sums_game_data/two.gif", cardback,
                 (card_posx, card_posy), (cw, ch), gamerunner)
     card.draw(screen)
     cards.append(card)
 
     card_posx += cw + s
-    card = Card("anger", "sums_game_data/three.gif", cardback,
+    card = Card(3, "sums_game_data/three.gif", cardback,
                 (card_posx, card_posy), (cw, ch), gamerunner)
     card.draw(screen)
     cards.append(card)
 
     card_posx += cw + s
-    card = Card("sadness", "sums_game_data/four.gif", cardback,
+    card = Card(4, "sums_game_data/four.gif", cardback,
                 (card_posx, card_posy), (cw, ch), gamerunner)
     card.draw(screen)
 
     cards.append(card)
 
-    # DRAW CARD HOLDERS #
-    # cardholder1 = CardHolder((300, 300), (cw, ch))
-    # cardholder1.draw(screen)
+    # SECOND LINE
+    second_line_h = h - 0.1 * h - ch
+
+    # SYMBOLS SIZES
+    cw2 = int(0.5 * cw)
+    ch2 = int(0.5 * ch)
+    # Distance from the sides = k
+    k = int(0.5 * (w - 4 * s - 3 * cw - 2 * cw2))
+
+    # DRAW SYMBOLS
+    card2_posx = k + cw + s
+    card2_posy = second_line_h + int(0.5 * (cw - cw2))
+    card2 = Card("plus", "sums_game_data/plus.jpg", cardback,
+                 (card2_posx, card2_posy), (cw2, ch2), gamerunner)
+    card2.can_open = False
+    card2.draw(screen)
+    cards.append(card2)
+
+    card2_posx = card2_posx + cw2 + 2 * s + cw
+    card2 = Card("equal", "sums_game_data/equal.png", cardback,
+                 (card2_posx, card2_posy), (cw2, ch2), gamerunner)
+    card2.can_open = False
+    card2.draw(screen)
+    cards.append(card2)
+
+    # DRAW CARDHOLDERS #
+    cardholder1_posx = k
+    cardholder1_posy = second_line_h
+    cardholder1 = Card("cardholder1", "sums_game_data/equal.png", cardback,
+                       (cardholder1_posx, cardholder1_posy), (cw, ch), gamerunner,
+                       cardholder=True)
+    cardholder1.can_open = False
+    cardholder1.draw(screen)
+    cards.append(cardholder1)
+
+    cardholder2_posx = cardholder1_posx + cw + cw2 + 2 * s
+    cardholder2_posy = cardholder1_posy
+    cardholder2 = Card("cardholder1", "sums_game_data/equal.png", cardback,
+                       (cardholder2_posx, cardholder2_posy), (cw, ch), gamerunner,
+                       cardholder=True)
+    cardholder2.can_open = False
+    cardholder2.draw(screen)
+    cards.append(cardholder2)
+
+    # LAST CARD 4
+    card3_posx = cardholder2_posx + cw + 2 * s + cw2
+    card3 = Card("equal", "sums_game_data/four.gif", cardback,
+                 (card3_posx, cardholder2_posy), (cw, ch), gamerunner)
+    card3.can_open = False
+    card3.draw(screen)
+    cards.append(card3)
+
+    # CORRECT AND WRONG BOXES
+    cory = int(h / 2 + (h - 0.2 * h - 2 * ch) / 4)
+    corx = k - s
+    corw = w - 2 * k + 2 * s
+    corh = int(ch + (h - 0.2 * h - 2 * ch) / 2)
+    cor = Card("correct", "sums_game_data/equal.png", cardback,
+               (corx, cory), (corw, corh), gamerunner, cardholder=True, color=(0, 0, 0))
+    cor.can_open = False
+    cor.draw(screen)
+    cards.append(cor)
+
+    # PASS THE CARDHOLDERS POSITIONS AS PARAMETERS
+    for card in cards:
+        card.cardholder_positions = ((cardholder1_posx, cardholder1_posy), (cardholder2_posx, cardholder2_posy))
+
+    # GAME DICTIONARY
+    game_dict = {'cardholders_full': 0, 'current_sum': 0}
 
     # add clock so that cpu does not go to 100%
     clock = pygame.time.Clock()
@@ -84,7 +150,8 @@ def start_sums_game(screen, gamerunner):
                 return
 
             for card in cards:
-                card.process_event(event, cards, screen)
+                card.process_event(event, cards, screen, game_dict=game_dict)
+                # print(game_dict)
 
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 redraw(cards, screen)
