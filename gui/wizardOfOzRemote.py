cc59f716552e3f69eb6e90026bd57a5928f6ab78
ngreek <ngreek54@gmail.com>
2018-09-24 13:20:34 +0300

    Add gui and zeno maven project
    Signed-off-by: ngreek <ngreek54@gmail.com>

---
+++ gui/wizardOfOzRemote.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from Tkinter import *
+from tkNotebook import Notebook
+import socket
+import sys
+
+class WizardOfOzRemote(object):
+
+
+    def __init__(self):
+        #Default settings
+        self.host = "192.168.0.127"
+        self.port = 1932
+
+        self.connect_to_broker()
+
+        #Create canvas
+        self.root = Tk()
+        self.root.title("Multi3 Admin")
+
+        #Create notebook
+        self.noteBook = Notebook(self.root, width=1800, height=700, activefg='blue')
+        self.noteBook.grid()
+
+        #Create main tab in notebook
+        self.sumsTab = self.noteBook.add_tab(text = "Sums Tab")
+
+        # self.drawSettingsTab()
+        self.sumsTab.focus()
+        
+        self.buttons = []
+        self.addSumsTab()
+
+    def add_button(self, tab, text, event, row, column, event_text=None, options=None, x=None, y=None, z=None, big=None, behavior=None):
+        if not big:
+            button = Button(tab, text=text, anchor=W, bg='grey', command= lambda: self.send_event(event, event_text, options,x,y,z,behavior))
+        else:
+            button = Button(tab, text=text, bg='grey', height=2, command= lambda: self.send_event(event, event_text, options,x,y,z,behavior))
+        button.grid(row=row, column=column, sticky='EW')
+        self.buttons.append(button)
+
+    def connect_to_broker(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect the socket to the port where the server is listening
+        server_address = (self.host, self.port)
+        self.sock.connect(server_address)
+
+        message = 'CONNECT furhat admin \n'
+        self.sock.sendall(message)
+
+        l = self.sock.recv(8192)
+
+        # message = 'SUBSCRIBE ** \n'
+        # self.sock.sendall(message)
+
+
+    def send_event(self, event, text=None, options=None, x=None, y=None, z=None, behavior=None):
+        json_format_1 = """{ \"class\": \"iristk.system.Event\", \"event_name\": \"%s\", \"text\": \"%s\" }\n"""
+        json_format_3 = """{ \"class\": \"iristk.system.Event\", \"event_name\": \"%s\", \"options\": \"%s\" }\n"""
+        json_format_4 = """{ \"class\": \"iristk.system.Event\", \"event_name\": \"%s\", \"x\": %f,  \"y\": %f,  \"z\": %f }\n"""
+        json_format_2 = """{ \"class\": \"iristk.system.Event\", \"event_name\": \"%s\"}\n"""
+        json_format_5 = """{ \"class\": \"iristk.system.Event\", \"event_name\": \"%s\", \"behavior\": \"%s\" }\n"""
+        event_format = "EVENT %s %s\n"
+        if options is not None:
+            js = json_format_3 % (event, options)
+            self.sock.sendall(event_format % (event, len(js)))
+            self.sock.sendall(js)
+            print "Sending event %s with text %s" % (event,  options) 
+            return       
+        if x is not None:
+            js = json_format_4 % (event, x,y,z)
+            self.sock.sendall(event_format % (event, len(js)))
+            self.sock.sendall(js)
+            print "Sending event %s with text %s" % (event,  options) 
+            return       
+
+        if behavior is not None:
+            js = json_format_5 % (event, behavior)
+            self.sock.sendall(event_format % (event, len(js)))
+            self.sock.sendall(js)
+            print "Sending event %s with behavior %s" % (event,  behavior) 
+            return       
+
+
+        if text == None:
+            js = json_format_2 % (event)
+            self.sock.sendall(event_format % (event, len(js)))
+            self.sock.sendall(js)
+            print "Sending event %s" % event
+        else:
+            js = json_format_1 % (event, text)
+            self.sock.sendall(event_format % (event, len(js)))
+            self.sock.sendall(js)
+            print "Sending event %s with text %s" % (event,  text)
+
+    def addSumsTab(self):
+        col = 1
+        Label(self.sumsTab, text="Goto State").grid(row=0, column=col)
+        self.add_button(self.sumsTab, row=1, column=col, text="Idle state", event="athena.games.sums.idle")
+        self.add_button(self.sumsTab, row=2, column=col, text="Introduction state", event="athena.games.sums.start")
+        self.add_button(self.sumsTab, row=3, column=col, text="Zeno default", event="athena.admin.zenodefault")
+        self.add_button(self.sumsTab, row=4, column=col, text="Zeno move", event="athena.admin.zenomovement")
+
+        col += 1
+        Label(self.sumsTab, text="Introduction State").grid(row=0, column=col)
+        self.add_button(self.sumsTab, row=1, column=col, text="Yes", event="athena.games.sums.start.respond",
+            event_text="yes")
+        self.add_button(self.sumsTab, row=2, column=col, text="No", event="athena.games.sums.start.respond",
+            event_text="no")
+        
+
+    ## RUN PROGRAM ##
+    def run(self):
+        #Starts and runs the GUI
+        self.root.mainloop()
+
+## AUTOSTART ##
+if __name__ == "__main__":
+    remote = WizardOfOzRemote()
+    remote.run()
